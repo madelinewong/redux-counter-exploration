ad79af44a60149460554b724b56f1f389c62a440
Madeline Wong <madelinegwong@outlook.com>
2018-03-02 18:36:09 -0500

    created redux store, a countReducer, and implmented increment and decrement actions, but the react app isn't working.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { createStore } from 'redux';
+
+//inital state 
+const initalState = 0;
+//reducer (how state changes)
+const countReducer = (state=initalState, action) => {
+  switch(action.type){
+    case "INCREMENT":
+      return state + 1; 
+    case "DECREMENT": 
+      return state - 1;
+      default: 
+      return state
+  }
+};
+//creation of the store
+const store = createStore(
+  countReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ 
+
 class App extends Component {
   render() {
-    return <div>hello</div>;
+    return <div>
+      <h1> {store.getState()}</h1>
+      <button onClick={() => store.dispatch({type: 'INCREMENT'})}>+</button>
+      <button onClick={() => store.dispatch({type: 'DECREMENT'})}>-</button>
+    </div>;
   }
 }
 
